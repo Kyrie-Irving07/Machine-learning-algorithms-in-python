15a0233a38d439ea0e177f579a2f9bb5e7faac20
HYH07007 <35869256+HYH07007@users.noreply.github.com>
2018-11-22 18:01:01 +0800

    Adversary

--- Deep Learning/GAN/GAN/GAN.py
+++ Deep Learning/GAN/GAN/GAN.py
@@ -3,7 +3,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data", one_hot = True)
 
 class G():
-    def _init__(self, input_dim, dim_seq):
+    def _init__(self, dim_seq):
         self.dim_seq = dim_seq
         self.depth = len(self.dim_seq)
         self.input_dim = self.dim_seq[0]
@@ -18,7 +18,35 @@ class G():
                 b = tf.get_variable("bias" + str(i), shape = [self.dim_seq[i + 1]])
                 input = tf.add(tf.matmul(input, w), b)
                 if i == self.depth - 2:
-                    input = tf.nn.tanh(input)
+                    input = tf.sigmoid(input)
                 else:
                     input = tf.nn.relu(input)
             return input
+
+class A():
+    def _init__(self, dim_seq):
+        self.depth = len(dim_seq)
+        self.dim_seq = dim_seq
+        self.input = tf.placeholder(dtype=tf.float32, shape=dim_seq[0])
+        self.output = self.conv("Adversary", self.input)
+
+    def conv(self, name, input):
+        with tf.variable_scope(name) as scope:
+            for i in range(self.depth - 1):
+                w = self.get_filter(i)
+                input = tf.nn.conv2d(input=input, w=w, strides=[1,1,1,1], padding="SAME")
+                b = self.get_bias(i, [tf.shape(input)[1], tf.shape(input)[2]])
+                input = tf.add(input, b)
+                if i == self.depth - 2:
+                    input = tf.nn.softmax(input)
+                else:
+                    input = tf.nn.relu(input)
+
+    def get_filter(self, layer):
+            w = tf.get_variable("filter"+str(layer), shape=self.dim_seq(layer), dtype=tf.float32,
+                                initializer=tf.random_normal_initializer)
+            return w
+
+    def get_bias(self, layer, shape):
+        return tf.get_variable("bias"+str(layer), shape=shape, dtype=tf.float32,
+                               initializer=tf.random_normal_initializer)
